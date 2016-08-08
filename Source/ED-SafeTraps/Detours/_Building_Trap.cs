7c6815f8b8be4c5c2dd395a60cd7483929ce2fcc
Jaxxa <jaxxa90@gmail.com>
2016-08-08 21:33:11 +1000

    Removing unneeded comments and logging.

--- Source/ED-SafeTraps/Detours/_Building_Trap.cs
+++ Source/ED-SafeTraps/Detours/_Building_Trap.cs
@@ -15,7 +15,7 @@ namespace EnhancedDevelopment.SafeTraps.Detours
 
         protected virtual float _SpringChance(Pawn p)
         {
-            Log.Message("Custom Spring Chance");
+            //Log.Message("Custom Spring Chance");
 
             if (p.Faction != null)
             {
@@ -30,66 +30,13 @@ namespace EnhancedDevelopment.SafeTraps.Detours
                 num *= 0.1f;
             return Mathf.Clamp01(num);
         }
-
-        //private void _Spring(Pawn p)
-        //{
-        //    Log.Message("Custom Spring");
-        //    SoundStarter.PlayOneShot(SoundDef.Named("DeadfallSpring"), (SoundInfo)this.Position);
-
-        //    if (p.Faction != null)
-        //    {
-        //        p.Faction.TacticalMemory.TrapRevealed(this.Position);
-        //    }
-
-        //    this.SpringSub(p);
-
-
-        //}
-
-        //private void _Tick()
-        //{
-        //    base.Tick();
-        //    if (this.Armed)
-        //    {
-        //        List<Thing> thingList = GridsUtility.GetThingList(this.Position);
-        //        for (int index = 0; index < thingList.Count; ++index)
-        //        {
-        //            Pawn p = thingList[index] as Pawn;
-        //            if (p != null && !this.touchingPawns.Contains(p))
-        //            {
-        //                this.touchingPawns.Add(p);
-        //                this.CheckSpring(p);
-        //            }
-        //        }
-        //    }
-        //    for (int index = 0; index < this.touchingPawns.Count; ++index)
-        //    {
-        //        Pawn pawn = this.touchingPawns[index];
-        //        if (!pawn.Spawned || pawn.Position != this.Position)
-        //            this.touchingPawns.Remove(pawn);
-        //    }
-        //}
+        
 
         protected override void SpringSub(Pawn p)
         {
             throw new NotImplementedException();
         }
-
-        //private void CheckSpring(Pawn p)
-        //{
-
-        //    Log.Message("Custom CheckSpring");
-
-        //    if ((double)Rand.Value >= (double)this.SpringChance(p))
-        //        return;
-        //    if (! p.Faction.IsPlayer)
-        //    {
-        //        this.Spring(p);
-        //    }
-        //    if (p.Faction !=  Faction.OfPlayer && p.HostFaction != Faction.OfPlayer)
-        //        return;
-        //    Find.LetterStack.ReceiveLetter(new Letter(Translator.Translate("LetterFriendlyTrapSprungLabel", (object)p.NameStringShort), Translator.Translate("LetterFriendlyTrapSprung", (object)p.NameStringShort), LetterType.BadNonUrgent, (TargetInfo)this.Position), (string)null);
-        //}
+        
 
     }
 }
